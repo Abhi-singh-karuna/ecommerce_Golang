a0d09190a17dabc94d26ea517c5743e3d80473ae
SHASHANK KESARWANI <52634856+amshashankk@users.noreply.github.com>
2021-10-18 14:30:39 +0530

    Updated User Auth

--- models/models.go
+++ models/models.go
@@ -1,8 +1,14 @@
 package models
 
+//--------------Created User struct for user registration-------------//
 type User struct {
 	Name     string `json:"name" bson:"name"`
 	Username string `json:"username" bson:"username"`
 	Email    string `json:"email" bson:"email"`
 	Password string `json:"password" bson:"password"`
+}
+
+//--------------Created Response struct to return the success/failure on signup-------------//
+type Response struct {
+	Message string `json:"message"`
 }
--- routes/routes.go
+++ routes/routes.go
@@ -1,10 +1,12 @@
 package routes
 
 import (
-	"github.com/Abhi-singh-karuna/Ronin/authentication"
+	"github.com/amshashankk/authentication"
 	"github.com/gofiber/fiber/v2"
 )
 
+
+//--------------Created Routing funtion for SignUp & login-------------//
 func Route(app fiber.Router) {
 	app.Post("/login", authentication.Login)
 	app.Post("/signup", authentication.SignUp)
